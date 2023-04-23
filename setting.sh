231d39e3fdc3e1ef82c3089c0c82d66a2fbeec53
tomkenta <tomkenta2014@gmail.com>
2023-04-23 22:00:11 +0900

    [MOD] brew installをansibleではなくbrewbundleから実行

--- setting.sh
+++ setting.sh
@@ -6,12 +6,13 @@ ANSIBLE_PATH=~/work/src/github.com/tomkenta/mac-setting
 ## install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-## install ansible
-brew install ansible
-
 ## clone repo
 git clone https://github.com/tomkenta/mac-setting.git ${ANSIBLE_PATH}
 
 ## ansible
 cd ${ANSIBLE_PATH}
+
+## install apps (ansible) by brew
+brew bundle
+
 ansible-playbook -i inventory/localhost localhost.yml
